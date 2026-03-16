b71153451585b5d00359fcd0e6b6900b68ca2579
szachmacik <maciej.koziej01@gmail.com>
2026-03-16 08:14:13 +0100

    fix: extract clearCookie spread to avoid esbuild error

--- server/routers.ts
+++ server/routers.ts
@@ -19,7 +19,8 @@ export const appRouter = router({
     me: publicProcedure.query(opts => opts.ctx.user),
     logout: publicProcedure.mutation(({ ctx }) => {
       const cookieOptions = getSessionCookieOptions(ctx.req);
-      ctx.res.clearCookie(COOKIE_NAME, { ...cookieOptions, maxAge: -1 });
+      const _clearOpts = Object.assign({}, cookieOptions);
+      ctx.res.clearCookie(COOKIE_NAME, _clearOpts);
       return { success: true } as const;
     }),
   
